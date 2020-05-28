d11e50ef74b1794a32d1d934314a83f17139171b
subeans <39542978+subeans@users.noreply.github.com>
2020-05-28 18:17:56 +0900

    Update run_bm.sh

--- benchmark/run_bm.sh
+++ benchmark/run_bm.sh
@@ -2,5 +2,5 @@
 batchsize=(1 2 4 8 16 32 64)
 
 for i in {0..6};
-    do python benchmark.py --testVGG16 --no_timeline --iter_benchmark=100 --batchsize=${batchsize[i]};
+    do python benchmark.py --testVGG16 --no_timeline --iter_benchmark=100 --batchsize=${batchsize[i]} --optimizer=sgd;
 done
