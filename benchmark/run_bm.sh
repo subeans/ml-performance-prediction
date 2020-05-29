c098e668e94b9ddec175a9397da88067ca33b172
subeans <39542978+subeans@users.noreply.github.com>
2020-05-29 16:44:16 +0900

    Update run_bm.sh

--- benchmark/run_bm.sh
+++ benchmark/run_bm.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 batchsize=(1 2 4 8 16 32 64 128 256 512)
-
-for i in {0..9};
-    do python benchmark.py --testVGG16 --no_timeline --iter_benchmark=100 --batchsize=${batchsize[i]} --optimizer=sgd;
+opti=(sgd adam adadelta adagrad rmsprop momentum)
+for p in ${opti[*]}; do
+    for i in ${batchsize[*]}; do
+    	python benchmark.py --testVGG16 --no_timeline --iter_benchmark=100 --batchsize=$i --optimizer=$p
+	done
 done
