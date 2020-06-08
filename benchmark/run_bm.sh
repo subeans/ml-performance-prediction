ed1e98fc9fe799043b8185624811d4f07d0e19db
subeans <39542978+subeans@users.noreply.github.com>
2020-06-08 19:25:30 +0900

    Update run_bm.sh

--- benchmark/run_bm.sh
+++ benchmark/run_bm.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+num = (1 2 3)
 batchsize=(1 2 4 8 16 32 64 128 256 512)
 opti=(sgd adam adadelta adagrad rmsprop momentum)
 for p in ${opti[*]}; do
     for i in ${batchsize[*]}; do
-    	python benchmark.py --testLenet --imgsize=28 --numclasses=10 --no_timeline --iter_benchmark=100 --batchsize=$i --optimizer=$p
+        for k in ${num[*]};do
+    	    python benchmark.py --testLenet --imgsize=28 --numclasses=10 --no_timeline --iter_benchmark=100 --batchsize=$i --optimizer=$p
 	done
+    done
 done
