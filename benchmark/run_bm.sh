c48a5b4977cae25f44931228955dda9f5fed0f5d
subeans <39542978+subeans@users.noreply.github.com>
2020-06-20 16:36:54 +0900

    Update run_bm.sh

--- benchmark/run_bm.sh
+++ benchmark/run_bm.sh
@@ -5,7 +5,7 @@ mv /usr/local/lib/python2.7/dist-packages/tensorflow/contrib/slim/nets.py /usr/l
 mv whitebox/nets.py /usr/local/lib/python2.7/dist-packages/tensorflow/contrib/slim
 
 
-num=(1)
+num=(1 2 3)
 batchsize=(1 2 4 8 16 32 64 128 256 512)
 opti=(sgd adam adadelta adagrad rmsprop momentum)
 for p in ${opti[*]}; do
