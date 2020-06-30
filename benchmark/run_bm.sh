dae00455e67df8c4ffab4031d94cb389c26d3c9e
subeans <39542978+subeans@users.noreply.github.com>
2020-06-30 14:17:44 +0900

    Update run_bm.sh

--- benchmark/run_bm.sh
+++ benchmark/run_bm.sh
@@ -4,9 +4,7 @@ mv whitebox/lenet.py /usr/local/lib/python2.7/dist-packages/tensorflow/contrib/s
 mv /usr/local/lib/python2.7/dist-packages/tensorflow/contrib/slim/nets.py /usr/local/lib/python2.7/dist-packages/tensorflow/contrib/slim/nets_old.py
 mv whitebox/nets.py /usr/local/lib/python2.7/dist-packages/tensorflow/contrib/slim
 
-printf "start"
-printf 'sstart'
-echo 'start'
+
 num=(1 2 3)
 batchsize=(1 2 4 8 16 32 64 128 256 512)
 opti=(sgd adam adadelta adagrad rmsprop momentum)
@@ -14,8 +12,7 @@ for p in ${opti[*]}; do
     for i in ${batchsize[*]}; do
         for k in ${num[*]}; do
     	    python benchmark.py --testLenet --imgsize=28 --numclasses=10 --no_timeline --iter_benchmark=100 --batchsize=$i --optimizer=$p
-	    echo 'Finish'>&finish.txt
-	    touch finish.txt
+	   
 	done
     done
 done
