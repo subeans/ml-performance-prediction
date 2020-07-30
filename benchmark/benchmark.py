8d10cf12bdf85f3d021f8d339c2d8570dcecbaa2
subeans <39542978+subeans@users.noreply.github.com>
2020-07-30 16:21:38 +0900

    Update benchmark.py

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -7,7 +7,7 @@ import os
 import argparse
 import tensorflow as tf
 import time
-from utils_tf import benchmark_matmul, benchmark_conv, benchmark_conv_mult, benchmark_VGG16 ,benchmark_Lenet
+from utils_tf import benchmark_matmul, benchmark_conv, benchmark_conv_mult, benchmark_VGG16 ,benchmark_Lenet,benchmark_vggsmall
 from utils_tf import run_benchmark
 
 
@@ -20,7 +20,8 @@ parser = argparse.ArgumentParser('Benchmarking different aspects of a machine le
 parser.add_argument('--testMatMul', action="store_true", default=False, help='Benchmark matrix multiplication')
 parser.add_argument('--testConv', action="store_true", default=False, help='Benchmark 2D convolution')
 parser.add_argument('--testVGG16', action="store_true", default=False, help='Benchmark training VGG-16 on sythetic data')
-parser.add_argument('--testLenet', action="store_true", default=False, help='Benchmark training VGG-16 on sythetic data')
+parser.add_argument('--testLenet', action="store_true", default=False, help='Benchmark training lenet on sythetic data')
+parser.add_argument('--testVGGsmall', action="store_true", default=False, help='Benchmark training vggsmall on sythetic data')
 
 # General parameters
 parser.add_argument('--num_gpu', type=int, default=1, help='Number of GPUs to use')
@@ -268,6 +269,59 @@ def main(_):
             bm_vgg16.run_timeline(logfile, args.batchsize)
         print("\n========================================\n\n")
     
+    #vggsmall
+     if args.testVGGsmall:
+        if args.logfile == '':
+            logfile = str('/results/benchmark_VGGsmall_%s_%s'
+                    %(args.device, time.strftime("%Y%m%d")))
+        else:
+            logfile = args.logfile
+
+        model = benchmark_vggsmall.VGGsmall(args)
+
+        train_op, vggsmall_graph = model.create_benchmark_op()
+
+        bm_vggsmall = run_benchmark.benchmark(
+                train_op,
+                args.iter_warmup,
+                args.iter_benchmark,
+                args.iter_timeline,
+                vggsmall_graph)
+
+
+        print("========================================\n")
+        print("Start training VGG-16")
+        print("Optimizer == " + args.optimizer )
+        timeUsed = bm_vggsmall.run_benchmark()
+
+        print("\nTraining VGG-16 (%dx%d pixel, float%d, batchsize %d): "
+                "%.3f ms per batch / %.3f images per sec , optimizer %s)"
+                % (args.imgsize,
+                args.imgsize,
+                args.precision,
+                args.batchsize,
+                timeUsed*1000,
+                args.batchsize/timeUsed, args.optimizer))
+
+        if not args.no_saving:
+            if not os.path.isfile('%s.csv'%logfile):
+                header = ('operation, imsize, precision (bits), batchsize,'
+                        'time per batch (ms), performance (img/sec), '
+                        'memory use (MB), comment\n')
+                f = open('%s.csv'%logfile,'a+')
+                f.write(header)
+                f.close()
+
+            if use_gpu:
+                mem = bm_vggsmall.get_memory_use()
+            else:
+                mem = 0
+            with open('%s.csv'%logfile,'a+') as f:
+                f.write(model.generate_logtext(timeUsed, mem))
+
+        if not args.no_timeline:
+            bm_vggsmall.run_timeline(logfile, args.batchsize)
+        print("\n========================================\n\n")
     
     # Lenet5     
     if args.testLenet:
