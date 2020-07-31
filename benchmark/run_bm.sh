504dab76f6f8ab44423f41f27d656f5a3071d8da
subeans <39542978+subeans@users.noreply.github.com>
2020-08-01 01:10:54 +0900

    Update run_bm.sh

--- benchmark/run_bm.sh
+++ benchmark/run_bm.sh
@@ -14,8 +14,8 @@ opti=(sgd adadelta adam)
 for p in ${opti[*]}; do
     for i in ${batchsize[*]}; do
         for k in ${num[*]}; do
-	    #python benchmark.py --testVGGsmall --no_timeline --iter_benchmark=100 --batchsize=$i --optimizer=$p
-    	    python benchmark.py --testLenet --imgsize=28 --numclasses=10 --no_timeline --iter_benchmark=100 --batchsize=$i --optimizer=$p
+	    python benchmark.py --testVGGsmall --no_timeline --iter_benchmark=100 --batchsize=$i --optimizer=$p
+    	    #python benchmark.py --testLenet --imgsize=28 --numclasses=10 --no_timeline --iter_benchmark=100 --batchsize=$i --optimizer=$p
 	    #python benchmark.py --testVGG16 --no_timeline --iter_benchmark=100 --batchsize=$i --optimizer=$p
 	done
 	
