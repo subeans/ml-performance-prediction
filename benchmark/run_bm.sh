261d87deb9220d053201cef1abc4624e741a9028
subeans <39542978+subeans@users.noreply.github.com>
2020-07-31 15:03:32 +0900

    Update run_bm.sh

--- benchmark/run_bm.sh
+++ benchmark/run_bm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-mv whitebox/lenet.py /usr/local/lib/python2.7/dist-packages/tensorflow/contrib/slim/python/slim/nets
-mv /usr/local/lib/python2.7/dist-packages/tensorflow/contrib/slim/nets.py /usr/local/lib/python2.7/dist-packages/tensorflow/contrib/slim/nets_old.py
-mv whitebox/nets.py /usr/local/lib/python2.7/dist-packages/tensorflow/contrib/slim
+#mv whitebox/lenet.py /usr/local/lib/python2.7/dist-packages/tensorflow/contrib/slim/python/slim/nets
+#mv /usr/local/lib/python2.7/dist-packages/tensorflow/contrib/slim/nets.py /usr/local/lib/python2.7/dist-packages/tensorflow/contrib/slim/nets_old.py
+#mv whitebox/nets.py /usr/local/lib/python2.7/dist-packages/tensorflow/contrib/slim
 
 
 num=(1 2 3)
